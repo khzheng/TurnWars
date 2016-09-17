5cbae6a6989c3cfc03a5d9943edcd18078c00d8e
Ken Zheng <kzheng@line2.com>
2016-09-17 13:13:43 -0700

    Initial Commit

---
+++ TurnWars/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  TurnWars
+//
+//  Created by Ken Zheng on 9/17/16.
+//  Copyright __MyCompanyName__ 2016. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
